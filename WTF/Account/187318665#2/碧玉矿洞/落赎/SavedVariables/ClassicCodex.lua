cf1ba0bb2271c6b7d64c2ee4396e3f6ff40da903
lztly <terry.lzt@gmail.com>
2019-10-17 23:06:07 +0800

    #6,配置文件新增ClassicCodex

---
+++ WTF/Account/187318665#2/碧玉矿洞/落赎/SavedVariables/ClassicCodex.lua
@@ -0,0 +1,30 @@
+
+CodexConfig = {
+	["showLowLevel"] = false,
+	["showHighLevel"] = true,
+	["colorBySpawn"] = true,
+	["autoAccept"] = false,
+	["minimumDropChance"] = 2,
+	["spawnMarkerSize"] = 10,
+	["nameplateIcon"] = false,
+	["autoTurnin"] = false,
+	["showFestival"] = false,
+	["currentQuestGivers"] = true,
+	["trackingMethod"] = 1,
+	["allQuestGivers"] = true,
+	["questMarkerSize"] = 14,
+}
+CodexHiddenQuests = {
+}
+CodexBrowserFavorites = {
+	["items"] = {
+	},
+	["units"] = {
+	},
+	["quests"] = {
+	},
+	["objets"] = {
+	},
+}
+CodexColors = {
+}
